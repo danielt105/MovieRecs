226fea4db96ed02afbdcb78e037759295b86555d
danielt105 <danielt105@gmail.com>
2016-10-24 09:16:40 -0700

    Basic Navdrawer functionality

--- MovieRecs/app/src/main/java/dltoy/calpoly/edu/movierecs/MainActivity.java
+++ MovieRecs/app/src/main/java/dltoy/calpoly/edu/movierecs/MainActivity.java
@@ -1,16 +1,35 @@
 package dltoy.calpoly.edu.movierecs;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
+
+    private NavigationView navView;
+    private Toolbar toolbar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.open_drawer, R.string.close_drawer);
+        drawer.addDrawerListener(toggle);
+        toggle.syncState();
+
+        navView = (NavigationView) findViewById(R.id.nav_view);
+        navView.setNavigationItemSelectedListener(this);
     }
 
     @Override
@@ -26,6 +45,8 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
             case R.id.watchlist:
                 break;
         }
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
         return displayAsSelectedItem;
     }
 }
