bdc8abccafbedd2e0e2fea3d456cf4afaa292394
danielt105 <danielt105@gmail.com>
2016-10-24 08:29:36 -0700

    Starting work on navdrawer

--- MovieRecs/app/src/main/java/dltoy/calpoly/edu/movierecs/MainActivity.java
+++ MovieRecs/app/src/main/java/dltoy/calpoly/edu/movierecs/MainActivity.java
@@ -1,13 +1,31 @@
 package dltoy.calpoly.edu.movierecs;
 
+import android.support.design.widget.NavigationView;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MenuItem;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    @Override
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+        boolean displayAsSelectedItem = true;
+        switch (id) {
+            case R.id.home:
+                break;
+            case R.id.advSearch:
+                break;
+            case R.id.watchlist:
+                break;
+        }
+        return displayAsSelectedItem;
     }
 }
