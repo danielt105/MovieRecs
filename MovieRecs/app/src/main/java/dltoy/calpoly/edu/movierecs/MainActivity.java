4ca2d338fa73479d28662333f07586e0d91e12ec
danielt105 <danielt105@gmail.com>
2016-10-24 09:28:04 -0700

    Setup default fragment setup for initial view
    also some comments

--- MovieRecs/app/src/main/java/dltoy/calpoly/edu/movierecs/MainActivity.java
+++ MovieRecs/app/src/main/java/dltoy/calpoly/edu/movierecs/MainActivity.java
@@ -1,40 +1,53 @@
 package dltoy.calpoly.edu.movierecs;
 
 import android.support.design.widget.NavigationView;
+import android.support.v4.app.Fragment;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity implements NavigationView.OnNavigationItemSelectedListener {
 
     private NavigationView navView;
     private Toolbar toolbar;
+//    private Fragment defaultFragment;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
+        //set up toolbar
         toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
+        //set up nav drawer
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
                 this, drawer, toolbar, R.string.open_drawer, R.string.close_drawer);
         drawer.addDrawerListener(toggle);
         toggle.syncState();
-
         navView = (NavigationView) findViewById(R.id.nav_view);
         navView.setNavigationItemSelectedListener(this);
+
+        //set default fragment and switch to it -- home screen
+//        switchToFragment();
+    }
+
+    //Switches the fragment in the activity
+    private void switchToFragment(int navId, int newTitle, Fragment newFragment) {
+        navView.setCheckedItem(navId);
+        toolbar.setTitle(newTitle);
+        getSupportFragmentManager().beginTransaction().replace(R.id.content, newFragment).commit();
     }
 
     @Override
     public boolean onNavigationItemSelected(MenuItem item) {
-        // Handle navigation view item clicks here.
         int id = item.getItemId();
         boolean displayAsSelectedItem = true;
         switch (id) {
@@ -44,7 +57,11 @@ public class MainActivity extends AppCompatActivity implements NavigationView.On
                 break;
             case R.id.watchlist:
                 break;
+            default:
+                Log.e("Nav drawer selection", "gave id: " + id);
         }
+
+        //close drawer after
         DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
         drawer.closeDrawer(GravityCompat.START);
         return displayAsSelectedItem;
